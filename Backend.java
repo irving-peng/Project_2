dd66c0deaf4015e326673202a7968d3c2a11e5f4
srikarv5 <60397318+srikarv5@users.noreply.github.com>
2021-03-22 13:38:06 -0500

    Add files via upload

--- Backend.java
+++ Backend.java
@@ -1,14 +1,21 @@
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 public class Backend extends RedBlackTree<Delivery>{
 
     RedBlackTree<Delivery> rbtree;
+    Calendar c = Calendar.getInstance();
 	
     //create red black tree from provided list of deliveries 
 	public Backend(ArrayList<Delivery> list) {
 		rbtree = new RedBlackTree<Delivery>();
+        
         for(Delivery del : list){
+            c.setTime(del.getOrderDate());
+            c.add(Calendar.DATE, del.getDaysToDeliver());
+            del.setDeliverOnDate(c.getTime());
             rbtree.insert(del);
         }
 	}
@@ -26,31 +33,50 @@ public class Backend extends RedBlackTree<Delivery>{
 
     //get the delivery object based on the item name
     public Delivery getDelivery(String itemName){
-        Delivery del = new Delivery();
-        return del;
+        Delivery del;
+        while(true){
+            try{
+                del = rbtree.iterator().next();
+            }
+            catch(NoSuchElementException e){
+                break;
+            }
+            if(del.getItemName()==itemName){
+                return del;
+            }
+        }
+        throw new NoSuchElementException("Delivery Item Not Found");
     }
 
     //get the delivery object based on the order date
     //we will assume there can only be one order per order date
     public Delivery getDelivery(Date orderDate){
-        Delivery del = new Delivery();
-        return del;
+        Delivery del = new Delivery("item", 10, 10, "53151", null, orderDate);
+        del.setOrderDate(orderDate);
+        return rbtree.get(del);
     }
 
-    public Delivery getDelivery(String itemName, Date orderDate){
-        Delivery del = new Delivery();
-        return del;
-    }
-
-    public boolean containsItem(String itemname){
-        return false;
+    public boolean containsItem(String itemName){
+        try{
+            getDelivery(itemName);
+        }
+        catch(NoSuchElementException e){
+            return false;
+        }
+        return true;
     }
 
     
     public boolean containsDate(Date deliveryDate){
-        Delivery del = new Delivery();
-        del.setOrderDate(deliveryDate);
-        return contains(del);
+        try{
+            getDelivery(deliveryDate);
+        }
+        catch(NoSuchElementException e){
+            return false;
+        }
+        return true;
     }
+
+
 
 }
