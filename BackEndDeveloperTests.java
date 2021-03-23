1da7c735dd86faa66192a985caee93812227cc32
srikarv5 <60397318+srikarv5@users.noreply.github.com>
2021-03-23 11:53:40 -0500

    Add files via upload

--- BackEndDeveloperTests.java
+++ BackEndDeveloperTests.java
@@ -15,9 +15,12 @@ public class BackendDeveloperTests {
         System.out.println("Test Get Delivery By Name: " + testGetDeliveryByName());
         System.out.println("Test Get Delivery By Date: " + testGetDeliveryByDate());
         System.out.println("Test Both Constructors: " + testBothConstructors());
+        System.out.println("Test Redundancies Handler: " + testAddDelivery());
+        System.out.println("Test Backend's toString(): " + testToString());
 
     }
 
+    //Checks the getDelivery(String itemName) method in the backend to ensure the front end can obtain any delivery object by its Item Name
     public boolean testGetDeliveryByName(){
         Backend backend;
         try{
@@ -40,6 +43,7 @@ public class BackendDeveloperTests {
 
     }
 
+    //Checks the getDelivery(Date orderDate) method in the backend to ensure the front end can obtain any delivery object by its Order Date
     public boolean testGetDeliveryByDate(){
         Backend backend;
         Date date;
@@ -63,6 +67,7 @@ public class BackendDeveloperTests {
         }
     }
 
+    //Tests both constructors of the backend class with different inputs and ensures they store the inputs correctly
     public boolean testBothConstructors(){
         Backend backend1;
         Backend backend2;
@@ -90,27 +95,58 @@ public class BackendDeveloperTests {
         }
     }
 
-
-    public boolean testRedundancies(){
+    //checks the addDelivery(Delivery del) method of the backend class to ensure deliveries can be added to the red black tree and stored correctly
+    public boolean testAddDelivery(){
         Backend backend;
         Date date;
         try{
             Path path = Paths.get("FinalData.csv");
             backend = new Backend(DataDeliveryReader.getDeliveryObjects(path));
             SimpleDateFormat dateInit = new SimpleDateFormat("dd/MM/yyyy");
-            date = dateInit.parse("01/11/2021");
+            date = dateInit.parse("01/01/2021");
         }
         catch(Exception e){
             System.out.print(e.getMessage());
             return false;
         }
-        Delivery d = null;
-        d = backend.getDelivery(date);
-        if(d.getItemName().equals("Delivery02") && d.getFeeDelivery()==5){
+        Delivery del = new Delivery("Delivery100", 10, 10, "53151", null, date);
+        try{
+            backend.addDelivery(del);
+        }
+        catch(IllegalArgumentException e){
+            return false;
+        }
+
+        if(backend.getDelivery("Delivery100")==del){
             return true;
         }
         else{
             return false;
         }
+    }
+
+    //tests that the toString() method of the backend class displays the correct data: the delivery item name and its corresponding order date
+    //This is vital for the frontend to display the information to the end user
+    public boolean testToString(){
+        Backend backend;
+
+        try{
+            Path path = Paths.get("FinalData.csv");
+            backend = new Backend(DataDeliveryReader.getDeliveryObjects(path));
+
+        }
+        catch(Exception e){
+            System.out.print(e.getMessage());
+            return false;
+        }
+
+        if(backend.toString().contains("Delivery38, 03/01/2021" + "\n")){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+
     }
 }   
